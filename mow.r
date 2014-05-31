3dd3214c64242775f2fef9f39421b23b3e93f960
Anna Stępień <solitaire@g.pl>
2014-05-31 17:21:04 +0200

    Poprawione przekształcanie atrybutów

--- mow.r
+++ mow.r
@@ -19,11 +19,11 @@ serviceMapping <- list("smtp"=0, "bgp"=1, "imap4"=2, "courier"=3, "name"=4, "exe
                        "http_443"=9, "discard"=10, "kshell"=11, "login"=12, "http"=13, "Z39_50"=14, "vmnet"=15, "supdup"=16,
                        "gopher"=17, "printer"=18, "aol"=19, "tftp_u"=20, "csnet_ns"=21, "http_8001"=22, "eco_i"=23, "time"=24,
                        "ssh"=25, "efs"=26, "hostnames"=27, "X11"=28, "klogin"=29, "sql_net"=30, "ldap"=31, "private"=32,
-                       "auth"=33, "uucp"=34, "pm_dump"=35, "link,ctf"=36, "IRC"=37, "ecr_i"=38, "netbios_ns"=39, "urp_i"=40,
-                       "pop_2"=41, "pop_3"=42, "rje"=43, "systat"=44, "ftp_data"=45,"finger"=45, "tim_i"=46, "remote_job"=47,
-                       "other"=48, "domain_u"=49, "urh_i"=50, "iso_tsap"=51, "netstat"=52, "daytime"=53, "whois"=54, "shell"=55,
-                       "mtp"=56, "sunrpc"=57, "uucp_path"=58, "red_i"=59, "harvest"=60, "nnsp"=61, "telnet"=62, "domain"=63,
-                       "ntp_u"=64, "netbios_dgm"=65, "nntp"=66, "netbios_ssn"=67)
+                       "auth"=33, "uucp"=34, "pm_dump"=35, "link"=36, "ctf"=37, "IRC"=38, "ecr_i"=39, "netbios_ns"=40, "urp_i"=41,
+                       "pop_2"=42, "pop_3"=43, "rje"=44, "systat"=45, "ftp_data"=46,"finger"=47, "tim_i"=48, "remote_job"=49,
+                       "other"=50, "domain_u"=51, "urh_i"=52, "iso_tsap"=53, "netstat"=54, "daytime"=55, "whois"=56, "shell"=57,
+                       "mtp"=58, "sunrpc"=59, "uucp_path"=60, "red_i"=61, "harvest"=62, "nnsp"=63, "telnet"=64, "domain"=65,
+                       "ntp_u"=66, "netbios_dgm"=67, "nntp"=68, "netbios_ssn"=69)
 # mapowanie pomiędzy flagą i jej ciągłym odpowiednikiem
 flagMapping <- list("REJ"=0, "SF"=1, "SH"=2, "RSTO"=3, "OTH"=4, "RSTR"=5, "RSTOS0"=6, "S0"=7, "S1"=8, "S2"=9, "S3"=10)
 
@@ -46,10 +46,18 @@ loadData <- function(path) {
 #
 # kolumna 42 - klasa ataku
 normalizeData <- function(data) {
-  data[,2] <- sapply(data[,2], function(x) protocolTypeMapping[[x]])
-  data[,3] <- sapply(data[,3], function(x) serviceMapping[[x]])
-  data[,4] <- sapply(data[,4], function(x) flagMapping[[x]])
-  data[,42] <- sapply(data[,42], function(x) classMapping[[x]])
+  for(i in 1:nrow(data)) {
+    data[i, 2] <- protocolTypeMapping[[data[i, 2]]]
+    data[i, 3] <- serviceMapping[[data[i, 3]]]
+    data[i, 4] <- flagMapping[[data[i, 4]]]
+    data[i, 42] <- classMapping[[data[i, 42]]]
+    print(i)
+  }
+  
+  data[,2] <- as.numeric(data[,2])
+  data[,3] <- as.numeric(data[,3])
+  data[,4] <- as.numeric(data[,4])
+  
   return (data)
 }
 
@@ -57,9 +65,9 @@ selectAttributes <- function(data) {
   #TODO
 }
 
-#TODO - dodać argumenty dla poszczególnych klasyfikatorów
-getSVMModel <- function(train, test, classes) {
-  model <- svm(classes ~ ., train, probability=TRUE, scale=FALSE)
+getSVMModel <- function(train, test, classes, kernelFunc, degree, gamma, coef0, cost) {
+  model <- svm(classes ~ ., train, probability=TRUE, scale=FALSE, kernel=kernelFunc, 
+               degree=kernelDegree, gamma=kernelGamma, coef0=kernelCoef0, cost=svmCost)
   
   predictions <- predict(model, test, probability=FALSE)
   rawPredictions <- attributes(predict(model, test, probability=TRUE))$probabilities
@@ -67,17 +75,48 @@ getSVMModel <- function(train, test, classes) {
   return (list(model=model, predictions=predictions, rawPredictions=rawPredictions))
 }
 
-getKNNModel <- function(train, test, classes) {
-  model <- knn(train, test, classes ~ ., k = 3, prob=TRUE)
+getKNNModel <- function(train, test, classes, knnK, KNNAlgorithm) {
+  model <- knn(train, test, classes, k=knnK, algorithm=KNNAlgorithm, prob=TRUE)
   
   return (list(model=model, predictions=model, rawPredictions=attr(model, "prob")))
 }
 
-getNaiveBayesModel <- function(train, test, classes) {
-  model <- naiveBayes(classes ~ ., train)
+getNaiveBayesModel <- function(train, test, classes, naiveBayesLaplace) {
+  model <- naiveBayes(classes ~ ., train, laplace=naiveBayesLaplace)
   
   predictions <- predict(model, test, type="class")
   rawPredictions <- predict(model, test, type="raw")
   
   return (list(model=model, predictions=predictions, rawPredictions=rawPredictions))
+}
+
+prepareData <- function(path, sampleSize) {
+  data <- loadData(path)
+  
+  # podział danych na zbiór trenujący i testowy
+  set.seed(1)
+  sampledData <- data[sample(nrow(data), sampleSize), ]
+  trainInd <- sample(seq_len(nrow(sampledData)), size = floor(0.8*nrow(sampledData)))
+  train <- sampledData[trainInd, ]
+  test <- sampledData[-trainInd, ]
+  
+  train <- normalizeData(train)
+  test <- normalizeData(test)
+  
+  lastCol = ncol(train) - 1
+  
+  return (list(trainData=train[,1:lastCol], trainClasses=train[,lastCol+1], testData=test[,1:lastCol], testClasses=test[,lastCol+1]))
+}
+
+#TODO - funkcje realizujące eksperymenty
+testSVM <- function() {
+  
+}
+
+testNaiveBayes <- function() {
+  
+}
+
+testKNN <- function() {
+  
 }
